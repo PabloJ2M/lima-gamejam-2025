7a3b0a2b0360f76fb49ce1f8e65054057a26cbb6
Pablo Mansilla <pablomansilla60@gmail.com>
2025-01-25 09:29:48 -0500

    created animations assets

--- Assets/Code/Scripts/Player/Character2D.cs
+++ Assets/Code/Scripts/Player/Character2D.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Character2D : MonoBehaviour
 {
     public Rigidbody2D body { get; private set; }
+    public Animator animator { get; private set; }
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 }
