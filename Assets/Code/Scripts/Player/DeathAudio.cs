40cf4608e66951aa0a68e619a1eee6dbf4f8fabf
HyperSM1 <francisco.12pretell@gmail.com>
2025-02-25 13:41:09 -0500

    Voz al morir

---
+++ Assets/Code/Scripts/Player/DeathAudio.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DeathAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip[] deathSounds;
+    [SerializeField] private bool enable;
+
+
+    private void Start()
+    {
+        enable = true;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void DeathMessage()
+    {
+        if (enable)
+        {
+            if (deathSounds.Length > 0)
+            {
+                AudioClip selectedClip = deathSounds[Random.Range(0, deathSounds.Length)];
+                audioSource.PlayOneShot(selectedClip);
+
+                enable = false;
+            }
+        }
+
+    }
+}
